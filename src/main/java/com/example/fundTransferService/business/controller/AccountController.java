72ad73acb94940d56c44d391f8e4530c05bd2be9
ismailab <Abdelrhman.Ismail@arhs-consulting.com>
2024-08-13 02:22:47 +0200

    adding swagger documentation

--- src/main/java/com/example/fundTransferService/business/controller/AccountController.java
+++ src/main/java/com/example/fundTransferService/business/controller/AccountController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.fundTransferService.business.dto.requests.AccountCreationRequest;
@@ -17,16 +18,27 @@ import com.example.fundTransferService.business.dto.response.FundsTransferRespon
 import com.example.fundTransferService.business.model.Account;
 import com.example.fundTransferService.business.service.AccountService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("account")
+@Tag(name="Accounts Resource")
 public class AccountController {
 
     private final AccountService accountService;
 
+    @Operation(summary = "Creates a new account for the referenced Account Holder",
+            description= "Account Holder must exist", parameters = @Parameter(
+            name =  "accountHolderId",
+            description  = "Account owner ID",
+            example = "23",
+            required = true))
+    @ResponseStatus(HttpStatus.CREATED)
     @PostMapping(value = "/add/{accountHolderId}", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<AccountCreationResponse> addNewAccount(@PathVariable Long accountHolderId, @RequestBody @Valid AccountCreationRequest accountCreationRequest) {
         Account account = accountService.createNewAccount(accountHolderId, accountCreationRequest);
@@ -34,7 +46,10 @@ public class AccountController {
         return new ResponseEntity<>(accountCreationResponse, HttpStatus.CREATED);
     }
 
+    @Operation(summary = "Transfer Funds between two accounts",
+            description= "Funds are transferred from the debited account to the credited account, if the credited account has a different currency then an exchange of currency operation is performed")
     @PostMapping(value = "/transfer", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(HttpStatus.OK)
     public ResponseEntity<FundsTransferResponse> transfer(@RequestBody @Valid FundsTransferRequest fundTransferRequest) {
         return new ResponseEntity<>(accountService.transfer(fundTransferRequest), HttpStatus.OK);
     }
--- src/main/java/com/example/fundTransferService/business/controller/AccountHolderController.java
+++ src/main/java/com/example/fundTransferService/business/controller/AccountHolderController.java
@@ -10,15 +10,19 @@ import org.springframework.web.bind.annotation.RestController;
 import com.example.fundTransferService.business.model.AccountHolder;
 import com.example.fundTransferService.business.service.AccountHolderService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("account-holder")
+@Tag(name = "Accounts Holder Resource")
 public class AccountHolderController {
 
     private final AccountHolderService accountHolderService;
 
+    @Operation(summary = "Lists All accounts in the system")
     @GetMapping(path = "/list")
     public List<AccountHolder> listAllAccountHolders() {
         return accountHolderService.getAllAccountHolders();
